e60eb248ea0fda466f00dc3012120d8196390647
Aaditya Mukherjee <aaditya.mukherjee2007@gmail.com>
2026-06-29 09:24:44 +0530

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,84 @@
+#include <Arduino.h>
+
+// Motor A
+int in1 = 25;
+int in2 = 26;
+
+// Motor B
+int in3 = 32;
+int in4 = 33;
+
+
+// put function declarations here:
+void forward();
+void backward();
+void forward_right();
+void forward_left();
+void stop();
+
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(in1, OUTPUT);
+  pinMode(in2, OUTPUT);
+  pinMode(in3, OUTPUT);
+  pinMode(in4, OUTPUT);
+
+  delay(3000);
+  forward();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  
+}
+
+// put function definitions here:
+void forward() {
+  digitalWrite(in1, HIGH);
+  digitalWrite(in2, LOW);
+  digitalWrite(in3, HIGH);
+  digitalWrite(in4, LOW);
+}
+
+void backward() {
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, HIGH);
+  digitalWrite(in3, LOW);
+  digitalWrite(in4, HIGH);
+}
+
+void forward_right() {
+  digitalWrite(in1, HIGH);
+  digitalWrite(in2, LOW);
+  digitalWrite(in3, LOW);
+  digitalWrite(in4, LOW);
+}
+
+void forward_left() {
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, LOW);
+  digitalWrite(in3, HIGH);
+  digitalWrite(in4, LOW);
+}
+
+void backward_right() {
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, HIGH);
+  digitalWrite(in3, LOW);
+  digitalWrite(in4, LOW);
+}
+
+void backward_left() {
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, LOW);
+  digitalWrite(in3, LOW);
+  digitalWrite(in4, HIGH);
+}
+
+void stop() {
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, LOW);
+  digitalWrite(in3, LOW);
+  digitalWrite(in4, LOW);
+}
