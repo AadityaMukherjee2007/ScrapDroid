b59ba98ae336d18d3764d116fcc0b8d8dd44b8e9
Aaditya Mukherjee <aaditya.mukherjee2007@gmail.com>
2026-07-05 12:34:14 +0530

    major updates and bug fixes for motor direction control

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,6 @@ int in2 = 26;
 int in3 = 32;
 int in4 = 33;
 
-
 // put function declarations here:
 void forward();
 void backward();
@@ -18,69 +17,128 @@ void backward_right();
 void backward_left();
 void stop();
 
-
-void setup() {
+void setup()
+{
   // put your setup code here, to run once:
   pinMode(in1, OUTPUT);
   pinMode(in2, OUTPUT);
   pinMode(in3, OUTPUT);
   pinMode(in4, OUTPUT);
 
-  Serial.begin(9600);
+  Serial.begin(115200);
 
-  delay(3000);
+  // delay(3000);
+  // forward();
+  // delay(2000);
+  // stop();
 }
 
-void loop() {
+void loop()
+{
   // put your main code here, to run repeatedly:
-  String data = Serial.readString();
-  Serial.println(data);
+  if (Serial.available() > 0)
+  {
+    String data = Serial.readStringUntil('\n');
+    data.trim();
+    // Serial.println(data);
+
+    // Forward & Backward
+    if (data == "f")
+    {
+      forward();
+    }
+    else if (data == "b")
+    {
+      backward();
+    }
+    // Forward Turns
+    else if (data == "fr")
+    {
+      forward_right();
+    }
+    else if (data == "fl")
+    {
+      forward_left();
+    }
+    // Backward Turns
+    else if (data == "br")
+    {
+      backward_right();
+    }
+    else if (data == "bl")
+    {
+      backward_left();
+    }
+    // Full Stop
+    else if (data == "s")
+    {
+      stop();
+    }
+    // Catch-all for unrecognized commands
+    else
+    {
+      Serial.println("Invalid command. Use: f, b, fr, fl, br, bl, s");
+    }
+  }
 }
 
 // put function definitions here:
-void forward() {
+void forward()
+{
+  stop();
   digitalWrite(in1, HIGH);
   digitalWrite(in2, LOW);
   digitalWrite(in3, HIGH);
   digitalWrite(in4, LOW);
 }
 
-void backward() {
+void backward()
+{
+  stop();
   digitalWrite(in1, LOW);
   digitalWrite(in2, HIGH);
   digitalWrite(in3, LOW);
   digitalWrite(in4, HIGH);
 }
 
-void forward_right() {
+void forward_left()
+{
+  stop();
   digitalWrite(in1, HIGH);
   digitalWrite(in2, LOW);
   digitalWrite(in3, LOW);
   digitalWrite(in4, LOW);
 }
 
-void forward_left() {
+void forward_right()
+{
+  stop();
   digitalWrite(in1, LOW);
   digitalWrite(in2, LOW);
   digitalWrite(in3, HIGH);
   digitalWrite(in4, LOW);
 }
 
-void backward_right() {
+void backward_left()
+{
+  stop();
   digitalWrite(in1, LOW);
   digitalWrite(in2, HIGH);
   digitalWrite(in3, LOW);
   digitalWrite(in4, LOW);
 }
 
-void backward_left() {
+void backward_right()
+{
+  stop();
   digitalWrite(in1, LOW);
   digitalWrite(in2, LOW);
   digitalWrite(in3, LOW);
   digitalWrite(in4, HIGH);
 }
 
-void stop() {
+void stop()
+{
   digitalWrite(in1, LOW);
   digitalWrite(in2, LOW);
   digitalWrite(in3, LOW);
